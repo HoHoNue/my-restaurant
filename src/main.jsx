9ee6424217d7897bd8f0caf5582d23abb4182709
HoHoNue <a01020441481@gmail.com>
2026-02-20 14:17:44 +0900

    업데이트 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+// 만약 index.css 파일이 없다면 아래 줄은 지워도 됩니다.
+import './index.css' 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// HTML의 id가 'root'인 요소에 우리가 만든 App 컴포넌트를 그려줍니다.
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
